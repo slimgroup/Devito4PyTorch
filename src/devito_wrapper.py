fix gradient

src/devito_wrapper.py
@@ -1,6 +1,5 @@
 import torch
 from torch.autograd import Function
-from examples.seismic.acoustic import AcousticWaveSolver
 import numpy as np
 import copy
 
@@ -8,10 +7,10 @@ import copy
 class ForwardBorn(Function):
 
     @staticmethod
-    def forward(ctx, input, model, geometry, device):
+    def forward(ctx, input, model, geometry, device, solver):
         input = input.to('cpu')
         # Save modeling parameters for backward pass
-        ctx.solver = AcousticWaveSolver(model, geometry, space_order=8)
+        ctx.solver = solver
         ctx.model = model
         ctx.geometry = geometry
         ctx.device = device
@@ -28,24 +27,25 @@ class ForwardBorn(Function):
     def backward(ctx, grad_output):
 
         grad_output = grad_output.detach().cpu().numpy()
-
+        rec = ctx.geometry.rec
+        rec.data[:] = grad_output[:]
         # Adjoint linearized modeling
         u0 = ctx.solver.forward(save=True)[1]
-        g = ctx.solver.gradient(grad_output, u0)[0].data
+        g = ctx.solver.gradient(rec, u0)[0].data
 
         # Remove padding
         nb = ctx.model.nbl
         g = torch.from_numpy(g[nb:-nb, nb:-nb]).to(ctx.device)
 
-        return g.view(1, 1, g.shape[0], g.shape[1]), None, None, None
+        return g.view(1, 1, g.shape[0], g.shape[1]), None, None, None, None
 
 class AdjointBorn(Function):
 
     @staticmethod
-    def forward(ctx, input, model, geometry, device):
+    def forward(ctx, input, model, geometry, device, solver):
 
         # Save modeling parameters for backward pass
-        ctx.solver = AcousticWaveSolver(model, geometry, space_order=8)
+        ctx.solver = solver
         ctx.model = model
         ctx.geometry = geometry
         ctx.device = device
@@ -70,14 +70,13 @@ class AdjointBorn(Function):
         # Linearized forward modeling
         d_lin = ctx.solver.born(input[0, 0, :, :])[0].data
 
-        return torch.from_numpy(d_lin).to(ctx.device), None, None, None
+        return torch.from_numpy(d_lin).to(ctx.device), None, None, None, None
 
 
 class ForwardModeling(Function):
 
     @staticmethod
-    def forward(ctx, input, model, geometry, device):
-
+    def forward(ctx, input, model, geometry, device, solver):
         # Save modeling parameters for backward pass
         # from IPython import embed; embed()
         ctx.model = model
@@ -87,9 +86,8 @@ class ForwardModeling(Function):
         # Prepare input
         input = torch.nn.ReplicationPad2d((ctx.model.nbl))(input).detach().cpu().numpy()
         ctx.model.vp.data[:] = input**(-0.5)
-        ctx.geometry.model = ctx.model
         
-        ctx.solver = AcousticWaveSolver(ctx.model, ctx.geometry, space_order=8)
+        ctx.solver = solver
         # Nonlinear forward modeling
         d_nonlin, ctx.u0 = ctx.solver.forward(save=True)[:2]
 
@@ -105,4 +103,4 @@ class ForwardModeling(Function):
         nb = ctx.model.nbl
         g = torch.from_numpy(g[nb:-nb, nb:-nb]).to(ctx.device)
 
-        return g.view(1, 1, g.shape[0], g.shape[1]), None, None, None
+        return g.view(1, 1, g.shape[0], g.shape[1]), None, None, None, None

src/main.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 from devito_wrapper import ForwardBorn, ForwardModeling
 from examples.seismic import demo_model, setup_geometry
+from examples.seismic.acoustic import AcousticWaveSolver
 from scipy import ndimage
 import matplotlib.pyplot as plt
 
@@ -21,9 +22,10 @@ class ForwardModelingLayer(torch.nn.Module):
         self.model = model
         self.geometry = geometry
         self.device = device
+        self.solver = AcousticWaveSolver(self.model, self.geometry, space_order=8)
 
     def forward(self, x):
-        data = self.forward_modeling.apply(x, self.model, self.geometry, self.device)
+        data = self.forward_modeling.apply(x, self.model, self.geometry, self.device, self.solver)
         return data
 
 
@@ -36,9 +38,10 @@ class ForwardBornLayer(torch.nn.Module):
         self.model = model
         self.geometry = geometry
         self.device = device
+        self.solver = AcousticWaveSolver(self.model, self.geometry, space_order=8)
 
     def forward(self, x):
-        data = self.forward_born.apply(x, self.model, self.geometry, self.device)
+        data = self.forward_born.apply(x, self.model, self.geometry, self.device, self.solver)
         return data
 
 
@@ -47,11 +50,12 @@ if __name__ == '__main__':
 
     tn = 1000.
     model = demo_model('layers-isotropic', origin=(0., 0.), shape=(101, 101),
-                          spacing=(10., 10.), nbl=40, grid=None, nlayers=5)
+                          spacing=(10., 10.), nbl=40, nlayers=5)
     model0 = demo_model('layers-isotropic', origin=(0., 0.), shape=(101, 101),
-                          spacing=(10., 10.), nbl=40, grid=model.grid, nlayers=5)
+                          spacing=(10., 10.), nbl=40, nlayers=5)
     model0.vp.data[:] = ndimage.gaussian_filter(model0.vp.data, sigma=(1, 1), order=0) 
-    geometry = setup_geometry(model0, tn)
+    geometry0 = setup_geometry(model0, tn)
+    geometry = setup_geometry(model, tn)
 
     nb = model.nbl
     m = np.float32(model.vp.data**(-2))[nb:-nb, nb:-nb]
@@ -62,14 +66,12 @@ if __name__ == '__main__':
     forward_modeling = ForwardModelingLayer(model, geometry, device)
     d_obs = forward_modeling(m)
     d_0 = forward_modeling(m0)
-    d_lin = d_obs - d_0
+    d_lin0 = d_obs - d_0
 
-    forward_born = ForwardBornLayer(model0, geometry, device)
+    forward_born = ForwardBornLayer(model0, geometry0, device)
     dm_est = torch.zeros(m.size(), requires_grad=True, device=device)
-    d_lin = forward_born(dm_est)
 
-
-    loss = 0.5*torch.norm(forward_born(dm_est) - d_obs)**2
+    loss = 0.5*torch.norm(forward_born(dm_est) - d_lin0)**2
     grad = torch.autograd.grad(loss, dm_est, create_graph=False)[0]
 
     plt.imshow(grad[0, 0, ...].detach().cpu().T); plt.show()